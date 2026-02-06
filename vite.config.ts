6791084e87c87a158ba094892c291273938775d3
abhi8618404 <a37477804@gmail.com>
2026-02-06 12:44:02 +0530

    Fix GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   server: {
     host: "::",
     port: 8080,
   },
+<<<<<<< HEAD
   build: {
     outDir: "docs",
     emptyOutDir: true,
@@ -16,10 +19,15 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === "development" && componentTagger(),
   ].filter(Boolean),
+=======
+
+>>>>>>> f7ad076 (Fix GitHub Pages deploy)
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  // ✅ IMPORTANT for GitHub Pages
   base: "/My-Portfolio-main/",
 }));
